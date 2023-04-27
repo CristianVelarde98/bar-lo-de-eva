bffc883d0654b3f1da1ba0b1d981282318177adb
Duvanrozop <rozoparraduvan@gmail.com>
2023-04-27 06:39:40 -0500

    new two endpoints singup and singin

--- api/src/Network/authentication.ts
+++ api/src/Network/authentication.ts
@@ -1,14 +1,18 @@
 import { Request, Response, Router } from 'express';
-// controllers
-import { Singin } from '@controllers/authentication';
-// Helpers
-import { sendServeHeader } from '@helpers/responseServer';
+import {
+  SingInController,
+  SingUpController,
+} from '@controllers/authentication';
+import { sendServer, sendServerWithHeader } from '@helpers/responseServer';
 
 const router = Router();
 
-router.post('/', (request: Request, response: Response) => {
-  const { gmail, password } = request.body;
-  sendServeHeader(response, () => Singin(gmail, password));
+router.post('/singup', async (request: Request, response: Response) => {
+  sendServer(response, () => SingUpController(request.body));
+});
+
+router.post('/signin', async (request: Request, response: Response) => {
+  sendServerWithHeader(response, () => SingInController(request.body));
 });
 
 export default router;
