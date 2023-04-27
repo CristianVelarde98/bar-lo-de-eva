d5dc769afa823eb1a23f3b2357a201a0ac546a5e
Duvanrozop <rozoparraduvan@gmail.com>
2023-04-27 06:38:49 -0500

    update for helpers

--- api/src/Helpers/responseServer.ts
+++ api/src/Helpers/responseServer.ts
@@ -1,14 +1,14 @@
 import { Response } from 'express';
 
-type controller = {
+export type send = {
   statusOk?: number;
   message?: string;
 };
 
-type controllerHeader = {
-  statusOk?: number;
-  message?: string;
-  token?: string;
+export type sendWithHeader = {
+  status?: number;
+  token: string;
+  messageSuccess?: string;
 };
 
 /**
@@ -20,31 +20,53 @@ type controllerHeader = {
  * @example
  * sendServe(response,() => controllador(body.user,body.password))
  */
-export function sendServe(response: Response, controller: Function) {
-  try {
-    const { statusOk, message }: controller = controller();
-    response.status(statusOk || 200).send(message || 'peticion sin mensage');
-  } catch (error: any) {
-    const customError = error.message.split('-');
-    response
-      .status(Number(customError[0]) || 500)
-      .send(customError[1] || 'error fatal server');
-  }
-}
-
-export function sendServeHeader(response: Response, controller: Function) {
-  try {
-    const { statusOk, token, message }: controllerHeader = controller();
-    response
-      .status(statusOk || 200)
-      .header('auth-token', token)
-      .send({
+export function sendServer(
+  response: Response,
+  controllerFn: () => Promise<send>
+) {
+  controllerFn()
+    .then(({ statusOk = 200, message = 'success' }) => {
+      response.status(Number(statusOk)).send({
         success: message,
       });
-  } catch (error: any) {
-    const customError = error.message.split('-');
-    response
-      .status(Number(customError[0]) || 500)
-      .send(customError[1] || 'error fatal server');
-  }
+    })
+    .catch((error: unknown) => {
+      const customError =
+        error instanceof Error ? error.message.split('-') : null;
+      const statusCode = customError ? Number(customError[0]) : 500;
+      const errorMessage = customError ? customError[1] : 'error fatal server';
+
+      response.status(statusCode).send(errorMessage);
+    });
+}
+
+/**
+ * Función que se encarga de dar respuesta concreta al frontend con el token
+ * @param {Response} response - response del network
+ * @param {Function} controller - controlador de la peticion
+ * @throws {Error} envia una respuesta con su status correspondiente y el mensaje del error de la peticion
+ * @description - si no quieres enviar status y/o mensaje de la peticion habran respuesta predeterminas para cuando no envies ninguna de las 2, pero deberas return un objeto vacio "return {}"
+ * @example
+ * sendServe(response,() => controllador(body.user,body.password))
+ */
+export function sendServerWithHeader(
+  response: Response,
+  controllerFn: () => Promise<sendWithHeader>
+) {
+  controllerFn()
+    .then(
+      ({ status = 200, token, messageSuccess = 'success' }: sendWithHeader) => {
+        response.status(Number(status)).header('auth-token', token).send({
+          success: messageSuccess,
+        });
+      }
+    )
+    .catch((error: unknown) => {
+      const customError =
+        error instanceof Error ? error.message.split('-') : null;
+      const statusCode = customError ? Number(customError[0]) : 500;
+      const errorMessage = customError ? customError[1] : 'error fatal server';
+
+      response.status(statusCode).send(errorMessage);
+    });
 }
--- api/src/Helpers/validate.ts
+++ api/src/Helpers/validate.ts
@@ -8,19 +8,43 @@ type ArgVery = {
   message: string;
 };
 
+/**
+ * isEmpty tiene como finalidad modularizar el detectar errores de parametros vacios
+ * @param arg - es un objeto el cual contiene los datos a verificar
+ * @throws - el directamente recorrera todo en busca de ver que esta vacio
+ * @description - es valido para type number y string
+ * @example
+ * function prueba(param1:string,param2:string,param3:string){
+ *  isEmpty({ param1, param2, param3 })
+ * }
+ */
 export function isEmpty(arg: Arg) {
-  for (const key in arg) {
-    if (arg[key].length === 0)
-      throw new Error(`400-El campo ${key} esta vacio`);
-  }
+  Object.keys(arg).forEach((key) => {
+    if (arg[key].length === 0) {
+      throw new Error(`400-El campo ${key} está vacío`);
+    }
+  });
 }
 
+/**
+ * regexVery busca validar por medio de regex y enviar un mensaje custom del error
+ * @param arg array de objetos el cual cada objeto contiene el rejex el value y por ultimo un mensaje personalizado
+ * @example
+ * regexVery([
+      {
+        property: nickname,
+        regex: /^[a-zA-Z0-9]{8,24}$/,
+        message: 'no contenga simbolos,que sea minimo 8 caracteres y maximo 24',
+      }
+    ]);
+ */
 export function regexVery(arg: ArgVery[]) {
-  for (const iterator of arg) {
+  arg.forEach((iterator) => {
     const validate = new RegExp(iterator.regex);
-    if (!validate.test(iterator.property))
+    if (!validate.test(iterator.property)) {
       throw new Error(
-        `400-El valor ${iterator.property} no es valido, ${iterator.message}`
+        `400-El valor ${iterator.property} no es válido, ${iterator.message}`
       );
-  }
+    }
+  });
 }
