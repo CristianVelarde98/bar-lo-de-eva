7f714997dc162793e85d8f9841bfd7aee2aa6e41
Duvanrozop <rozoparraduvan@gmail.com>
2023-04-27 06:43:55 -0500

    new doc for routes and new endpoints

--- api/src/routes/index.routes.ts
+++ api/src/routes/index.routes.ts
@@ -1,10 +1,9 @@
 import { Router, Express } from 'express';
-import authentication from '@network/authentication';
-// eslint-disable-next-line import/no-unresolved
-import userRouter from './user.route';
-// * Networks
 
-// mas facil y escalable segun yo
+// * Networks
+import authentication from '@network/authentication';
+import userRouter from './user.route';
+
 type tipadoRouter = {
   path: string;
   router: Router;
@@ -15,6 +14,14 @@ const routes: tipadoRouter[] = [
   { path: '/users', router: userRouter },
 ];
 
+/**
+ * Funcion encargada de crear el router
+ * @param server - recibe el app.express() para crear las routes
+ * @description mediante el objeto routes ella podra crear las rutas de forma mas dinamica
+ * @example
+ * const app = express();
+ * configureRoutes(app);
+ */
 function configureRoutes(server: Express) {
   routes.forEach(({ path, router }) => {
     server.use(path, router);
