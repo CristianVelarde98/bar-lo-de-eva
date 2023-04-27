74a70e6e70688d80d7b545f28b529d763a08f282
Duvanrozop <rozoparraduvan@gmail.com>
2023-04-27 06:40:10 -0500

    logic for mongo

--- api/src/Store/authentication.ts
+++ api/src/Store/authentication.ts
@@ -1,29 +1,23 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-import { ModelAuthSchema } from '@/Models/authentication';
-
-type Authentication = {
-  gmail: string;
-  password: string;
-};
-
-export type returnResponse = {
-  status?: number;
-  token?: string;
-  messageSuccess?: string;
-};
+import { ModelAuthSchema, IAuth } from '@/Models/authentication';
+import type { SingInUser, SingUpUser } from '@/Controllers/authentication';
+import type { sendWithHeader, send } from '@/Helpers/responseServer';
 
 dotenv.config();
 // ~ ENV
 const secrect = process.env.SECRET_KEY;
 
 // * ENCARGADA DE LOGEAR
-export async function signinStore({ gmail, password }: Authentication) {
+export async function signinStore({
+  nickname,
+  password,
+}: SingInUser): Promise<sendWithHeader> {
   try {
     if (secrect === undefined)
       throw new Error('500-Error con la bases de datos');
 
-    const exists = await ModelAuthSchema.findOne({ gmail });
+    const exists = await ModelAuthSchema.findOne({ nickname });
     if (!exists) throw new Error('400-Usuario no existe');
 
     const passwordCorrect: boolean = await exists.validatePassword(password);
@@ -33,13 +27,41 @@ export async function signinStore({ gmail, password }: Authentication) {
       expiresIn: 60 * 60 * 6,
     });
 
-    const response: returnResponse = {
+    const response: sendWithHeader = {
       status: 200,
       token,
       messageSuccess: 'Logeado correctamente',
     };
     return response;
-  } catch (error: any) {
-    throw new Error(error.message);
+  } catch (error: unknown) {
+    if (error instanceof Error) throw new Error(error.message);
+    else throw new Error('500-Error desconocido');
   }
+}
+
+// * ENCARGADA DE REGISTRAR
+export async function singUpStore({
+  nickname,
+  password,
+  confirmPassword,
+}: SingUpUser): Promise<send> {
+  if (secrect === undefined) throw new Error('500-Error con la bases de datos');
+
+  const isExists = await ModelAuthSchema.findOne({ nickname });
+  if (isExists?.nickname === nickname)
+    throw new Error(`400-El usuario ${nickname} ya existe`);
+
+  const newUser: IAuth = new ModelAuthSchema({
+    nickname,
+    password,
+    confirmPassword,
+  });
+
+  newUser.password = await newUser.encryptPassword(newUser.password);
+  await newUser.save();
+  const response: send = {
+    statusOk: 200,
+    message: `Usuario ${nickname} ha sido creado sastifactoriamente.`,
+  };
+  return response;
 }
