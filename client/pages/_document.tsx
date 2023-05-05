3a679a9062ae3fa795e015f436c7e98ef0ccdad9
Duvanrozop <rozoparraduvan@gmail.com>
2023-05-04 23:15:12 -0500

    update component father's

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,14 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-// * Components
-import NavbarDashboard from '@/components/navbar';
-
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body className='w-screen h-screen flex flex-row bg-lime-600'>
-        <NavbarDashboard />
+      <body className='w-screen h-screen flex flex-row'>
         <Main />
         <NextScript />
       </body>
