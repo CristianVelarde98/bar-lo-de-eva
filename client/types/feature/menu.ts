512315ce48505879333d9235f4d10bc3ce7495f6
Duvanrozop <rozoparraduvan@gmail.com>
2023-06-18 20:16:56 -0500

    feat: estructurando y apartando los types

---
+++ client/types/feature/menu.ts
@@ -0,0 +1,9 @@
+import { TColumnMenu } from '../services/menu';
+
+export type TDashItemsColumnsProps = {
+  dataset: TColumnMenu;
+};
+
+export type TDashColumnsProps = {
+  column: TColumnMenu[];
+};
