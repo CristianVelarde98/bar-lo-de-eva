5e701e3213f3ad18c8d8119b487b30e3940e1077
Duvanrozop <rozoparraduvan@gmail.com>
2023-06-22 01:22:48 -0500

    feat: new dependecies and change structure project

--- client/types/feature/menu.ts
+++ client/types/feature/menu.ts
@@ -1,9 +1,11 @@
 import { TColumnMenu } from '../services/menu';
 
 export type TDashItemsColumnsProps = {
+  column: string;
   dataset: TColumnMenu;
 };
 
 export type TDashColumnsProps = {
-  column: TColumnMenu[];
+  column: string;
+  dataset: TColumnMenu[];
 };
