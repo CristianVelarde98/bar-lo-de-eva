437b23a5ead7d7418a2729a34f241c003ab81f80
Duvanrozop <rozoparraduvan@gmail.com>
2023-06-22 01:24:45 -0500

    feat: change structure components

--- client/components/feature/Dash_Column.tsx
+++ client/components/feature/Dash_Column.tsx
@@ -1,14 +1,84 @@
-/* eslint-disable no-underscore-dangle */
 import React from 'react';
-import { TDashColumnsProps } from '@/types/feature/menu';
+import * as yup from 'yup';
+import { CopyPlus } from 'lucide-react';
 import DashItemsColumns from './Dash_ItemsColumns';
+import { TDashColumnsProps } from '@/types/feature/menu';
+import { Button } from '@/ui/button';
+import WindowsForm from '@/shared/WindowsForm';
+import InputsText from '@/shared/Inputs/InputsText';
+import useWindowsModel from '@/Hooks/useWindowsModel';
 
-const DashColumn: React.FC<TDashColumnsProps> = ({ column }) => {
+// TODO: mover los tipados para
+type TItemMenu = {
+  nombre: '';
+  descripcion: '';
+  precio: 0;
+};
+
+const DashColumn: React.FC<TDashColumnsProps> = ({ dataset, column }) => {
+  const [isOpen, handleOpen, handleClose] = useWindowsModel(false);
+
+  const handleSubmit = (values: TItemMenu) => {
+    // eslint-disable-next-line no-console
+    console.log(
+      '🚀 ~ file: Dash_Column.tsx:17 ~ handleSubmit ~ values: ',
+      values
+    );
+    handleClose();
+  };
+  const initialValues: TItemMenu = {
+    nombre: '',
+    descripcion: '',
+    precio: 0,
+  };
+  const validatorSchema = yup.object().shape({
+    nombre: yup.string().required('El nombre es requerido'),
+    descripcion: yup.string().required('La descripción es requerida'),
+    precio: yup
+      .number()
+      .required('El precio es requerido')
+      .positive('El precio debe ser mayor a 0'),
+  });
+
   return (
-    <section className='flex flex-col gap-2'>
-      {column.map((item) => (
-        <DashItemsColumns key={item._id} dataset={item} />
+    <section className='h-max flex flex-col gap-2'>
+      {dataset.map((item) => (
+        // eslint-disable-next-line no-underscore-dangle
+        <DashItemsColumns key={item._id} dataset={item} column={column} />
       ))}
+      <Button
+        onClick={handleOpen}
+        className='gap-5 font-jejumyeongjo font-bold text-base p-3 bg-green-600 hover:scale-105 hover:bg-green-600 transform transition-all'
+      >
+        <CopyPlus className='text-white' /> Agregar nuevo Producto
+      </Button>
+
+      <WindowsForm
+        className='w-2/5'
+        title={column}
+        initialValues={initialValues}
+        validationSchema={validatorSchema}
+        isOpen={isOpen}
+        onClose={handleClose}
+        handleSubmit={handleSubmit}
+      >
+        <InputsText
+          name='nombre'
+          label='Nombre:'
+          placeholder='Ingrese el nombre del producto'
+        />
+        <InputsText
+          name='descripcion'
+          label='Descripcion:'
+          placeholder='Ingrese una descripcion del producto'
+        />
+        <InputsText
+          type='number'
+          name='precio'
+          label='Precio:'
+          placeholder='Ingrese el precio del producto'
+        />
+      </WindowsForm>
     </section>
   );
 };
--- client/components/feature/Dash_ItemsColumns.tsx
+++ client/components/feature/Dash_ItemsColumns.tsx
@@ -1,10 +1,29 @@
+/* eslint-disable no-underscore-dangle */
 import React, { FC } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { Edit, XCircle } from 'lucide-react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
 import type { TDashItemsColumnsProps } from '@/types/feature/menu';
+import { Button } from '../ui/button';
+import { deleteMenu } from '@/services/api';
 
 const DashItemsColumns: FC<TDashItemsColumnsProps> = ({ dataset }) => {
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation({
+    mutationFn: deleteMenu,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['menu']);
+    },
+  });
+
   return (
-    <Card className='w-full min-w-[320px] max-w-sm bg-gray-900 border-gray-900'>
+    <Card className='w-full min-w-[320px] max-w-sm bg-gray-900 border-gray-900 p-1'>
       <CardHeader>
         <CardTitle className='flex flex-row items-end leading-10 font-normal font-jejumyeongjo whitespace-nowrap gap-2 text-xl text-yellow-400'>
           {dataset.nombre}
@@ -18,6 +37,18 @@ const DashItemsColumns: FC<TDashItemsColumnsProps> = ({ dataset }) => {
       <CardContent className='text-white text-base font-medium font-kellyslab'>
         <p>{dataset.descripcion}</p>
       </CardContent>
+      <CardFooter className='p-2 flex flex-row justify-end gap-1'>
+        <Button>
+          <Edit className='text-green-600' />
+        </Button>
+        <Button
+          onClick={() => {
+            deleteMutation.mutate(dataset._id);
+          }}
+        >
+          <XCircle className='text-red-600' />
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
