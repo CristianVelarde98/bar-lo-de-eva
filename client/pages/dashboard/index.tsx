55936429bd2b29ef8b1a91b7c31ca4e0da219a34
DuvanRozoP <rozoparraduvan@gmail.com>
2023-07-12 02:28:16 +0200

    feat: update componentes

--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-// * Components
-import LayoutNavbar from '@/components/Layout/LayoutNavbar';
+import Carousel from '@/components/shared/carousel';
+import { Input } from '@/components/ui/input';
 
 function Dashboard() {
   return (
-    <LayoutNavbar>
-      <section className='w-full h-full flex-col flex items-center justify-center bg-lime-600'>
-        <h1>Bienvenido al panel de administracion</h1>
-        <section className='w-3/4 h-3/4 bg-rose-500 rounded-3xl flex justify-center'>
-          <p className='m-3'>
-            ultimos cambios realizados X personas cambiaron a las y tanto horas.
-          </p>
-        </section>
+    <section className='w-full h-full flex-col flex items-center justify-center bg-lime-600'>
+      <h1>Bienvenido al panel de administracion</h1>
+      <section className='w-3/4 h-3/4 bg-rose-500 rounded-3xl flex justify-center'>
+        <Carousel>
+          <section>
+            <Input
+              accept='image/*'
+              id='picture'
+              type='file'
+              className='cursor-pointer'
+            />
+          </section>
+        </Carousel>
+        <p className='m-3'>
+          ultimos cambios realizados X personas cambiaron a las y tanto horas.
+        </p>
       </section>
-    </LayoutNavbar>
+    </section>
   );
 }
 
