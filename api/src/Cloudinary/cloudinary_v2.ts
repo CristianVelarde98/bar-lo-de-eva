ebebdf40d845dc5998517e65e6a80a09cf67ae6b
DuvanRozoP <rozoparraduvan@gmail.com>
2023-08-26 03:07:23 -0500

    feat: config cloudinary

---
+++ api/src/Cloudinary/cloudinary_v2.ts
@@ -0,0 +1,41 @@
+import { findErrorPropertiesAsync, isEmptyAsync } from '@/Helpers/errors';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_v2_NAME,
+  api_key: process.env.CLOUDINARY_v2_API_KEY,
+  api_secret: process.env.CLOUDINARY_v2_API_SECRET,
+});
+
+export async function uploadImageCloudinary(filePath: string) {
+  try {
+    const upload = await cloudinary.uploader.upload(filePath, {
+      folder: 'replit',
+    });
+
+    await Promise.all([
+      isEmptyAsync([upload]),
+      findErrorPropertiesAsync(['url', 'secure_url'], upload),
+    ]);
+
+    return {
+      url: upload.url,
+      publicId: upload.public_id,
+    };
+  } catch (error: unknown) {
+    if (error instanceof Error) throw new Error(error.message);
+    else throw new Error('error inespedado con cloudinary');
+  }
+}
+
+export async function deleteImageCloudinary(publicId: string) {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error: unknown) {
+    if (error instanceof Error)
+      throw new Error(
+        `Error al eliminar la imagen: ${publicId} - ${error.message}`
+      );
+    else throw new Error(`Error inesperado al eliminar la imagen: ${publicId}`);
+  }
+}
